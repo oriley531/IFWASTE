fbd77219f1200a4a50aa9c9735a42964247c3e7c
oriley531 <rorr.prof@gmail.com>
2023-07-31 15:30:35 -0400

    Working on the data2viz

--- pictures.R
+++ pictures.R
@@ -1,4 +1,12 @@
-library("tidyverse")
+# library
+library(tidyverse)
+
+# Pull the data
 bought <- read.csv("outputs/bought_food_1.csv")
 eaten <- read.csv("outputs/eaten_food_1.csv")
 wasted <- read.csv("outputs/wasted_food_1.csv")
+
+# Make the totals bargraph
+b_sums <- bought %>%
+    group(Type, Day.Bought) %>%
+    summarise(Sum_Value = sum(kg))
